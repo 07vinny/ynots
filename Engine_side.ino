63cc76ad3885f7720d4ffc601b27ed98ccd637e6
07vinny <49111407+07vinny@users.noreply.github.com>
2019-03-31 09:33:30 +0530

    Add files via upload

---
+++ Engine_side.ino
@@ -0,0 +1,30 @@
+#define a 6
+int b;
+void setup()
+{ 
+  Serial.begin(9600);
+  pinMode(a,OUTPUT);
+}
+void loop()
+{
+  if(Serial.available())
+  {
+  b=Serial.read();
+  if(b==1)
+  { 
+    Serial.print(b);
+    Serial.print("You are drunk");
+    digitalWrite(a,LOW);
+    delay(1000);
+  }
+  else if(b==0)
+  {
+    Serial.print(b);
+    Serial.print("Normal");
+    digitalWrite(a,HIGH);
+    delay(1000);
+  } 
+  }
+  
+}
+
---
+++ Helmet_side.ino
@@ -0,0 +1,42 @@
+const int output=7;
+const int analogInPin = A0;  
+const int analogOutPin = 9; 
+int sensorvalue = 0;       
+int outputValue = 0;        
+void setup() {
+ 
+  Serial.begin(9600);
+  pinMode(output,OUTPUT);
+}
+
+void loop() {
+  sensorvalue = analogRead(analogInPin);
+  outputValue = map(sensorvalue, 0, 1023, 0, 100);
+  analogWrite(analogOutPin, outputValue);
+
+
+  Serial.print("sensor = ");
+  if(sensorvalue<200)
+  {
+      digitalWrite(output,LOW);
+      Serial.write(output);
+  }
+  if (sensorvalue>=200 && sensorvalue<280)
+  {
+       digitalWrite(output,LOW);
+      Serial.write(output);
+ 
+  }
+  if (sensorvalue>=280 && sensorvalue<350)
+  {
+       digitalWrite(output,LOW);
+      Serial.write(output);
+  
+  }
+  if (sensorvalue>=350 && sensorvalue <450)
+  {
+      
+       digitalWrite(output,HIGH);
+      Serial.write(output);
+  
+  
